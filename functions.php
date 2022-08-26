a3b176659f314ad8e3c42db2b78b72acfa6f7270
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-08-26 07:54:17 +0200

    minor changes

--- functions.php
+++ functions.php
@@ -12,22 +12,22 @@
 
 if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_support() {
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @since Twenty Twenty-Two 1.0
+     *
+     * @return void
+     */
+    function twentytwentytwo_support() {
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
+        // Add support for block styles.
+        add_theme_support( 'wp-block-styles' );
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+        // Enqueue editor styles.
+        add_editor_style( 'style.css' );
 
-	}
+    }
 
 endif;
 
@@ -35,29 +35,29 @@ add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 
 if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_styles() {
-		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get( 'Version' );
+    /**
+     * Enqueue styles.
+     *
+     * @since Twenty Twenty-Two 1.0
+     *
+     * @return void
+     */
+    function twentytwentytwo_styles() {
+        // Register theme stylesheet.
+        $theme_version = wp_get_theme()->get( 'Version' );
 
-		$version_string = is_string( $theme_version ) ? $theme_version : false;
-		wp_register_style(
-			'twentytwentytwo-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			$version_string
-		);
+        $version_string = is_string( $theme_version ) ? $theme_version : false;
+        wp_register_style(
+            'twentytwentytwo-style',
+            get_template_directory_uri() . '/style.css',
+            array(),
+            $version_string
+        );
 
-		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'twentytwentytwo-style' );
+        // Enqueue theme stylesheet.
+        wp_enqueue_style( 'twentytwentytwo-style' );
 
-	}
+    }
 
 endif;
 
